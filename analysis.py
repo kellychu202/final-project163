54f1af6461faae49a24e5599836b4a6df440842b
Kelly Chu <kellychu202@gmail.com>
2020-08-16 20:52:38 -0700

    Concat movies and tv dataset

--- analysis.py
+++ analysis.py
@@ -1,21 +1,24 @@
 import pandas as pd
 
+
 def load_movs_shows(path_one, path_two):
     """
-    Return joined dataset between two given files, one with movies and one with
-    tv shows info, keeps all rows and columns from both datasets missing values
-    are NaN
+    Return combined dataset between two given files, one with movies and one
+    with tv shows info, keeps all rows and columns from both datasets
+    missing values are NaN
     """
     movies = pd.read_csv(path_one)
     shows = pd.read_csv(path_two)
-    movs_shows = movies.merge(shows, on='Title', how='outer')
+    # concat stacks the two datasets, no duplicate column names
+    movs_shows = pd.concat([movies, shows], axis=0, sort=True)
     return movs_shows
+
 
 def load_imdb(path_one, path_two):
     """
-    Return the joined dataset between two given files, one with imdb and one with 
-    movie info returned dataset only has rows where corresponding movie titles match
-    in the given two datasets
+    Return the joined dataset between two given files, one with imdb and
+    one with movie info returned dataset only has rows where corresponding
+    movie titles match in the given two datasets
     Resulting dataset has information of movies present in both given datasets
     """
     imdb = pd.read_csv(path_one)
@@ -25,18 +28,21 @@ def load_imdb(path_one, path_two):
 
 
 def main():
-    movs_shows = load_movs_shows('data/Movies_Streaming_Platforms.csv', 'data/TvShows_Streaming_Platforms.csv')
+    movs_shows = load_movs_shows('data/Movies_Streaming_Platforms.csv',
+                                 'data/TvShows_Streaming_Platforms.csv')
     # test
     print(movs_shows.head())
     print(movs_shows.columns)
     print(movs_shows.shape)
     # end test
-    imdb = load_imdb('data/IMDB_movies.csv', 'data/Movies_Streaming_Platforms.csv')
+    imdb = load_imdb('data/IMDB_movies.csv',
+                     'data/Movies_Streaming_Platforms.csv')
     # test
     print(imdb.head())
     print(imdb.columns)
     print(imdb.shape)
     # end test
+
 
 if __name__ == '__main__':
     main()
