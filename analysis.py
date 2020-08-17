e456498c3d630227277e32ecbbdce93f7c2f5e3f
Kelly Chu <kellychu202@gmail.com>
2020-08-16 22:14:30 -0700

    working on avg_rating

--- analysis.py
+++ analysis.py
@@ -27,6 +27,28 @@ def load_imdb(path_one, path_two):
     return merged
 
 
+def avg_rating(data):
+    """
+    Calculate average between rotten tomatoes (converted to 0-10 scale) and
+    imdb rating for each movie and tv show and 
+    Return (graphical representation) of overall average ratings for each
+    streaming platform
+    """
+    # Convert rotten tomatoes to 1-10 scale to match imdb scale
+    avg_rating = data[['Title', 'IMDb', 'Rotten Tomatoes', 'Netflix', 'Hulu', 'Prime Video', 'Disney+']]
+    avg_rating = avg_rating.dropna()
+    avg_rating['Rotten Tomatoes'] = avg_rating['Rotten Tomatoes'].str.rstrip('%s')
+    avg_rating['Rotten Tomatoes'] = avg_rating['Rotten Tomatoes'].astype(int) / 10
+    # average imdb and rotten tomatoe rating for each movie/tv show
+    avg_rating['Average Rating'] = (avg_rating['IMDb'] + avg_rating['Rotten Tomatoes']) / 2
+    # dict maps streaming platform to overall average rating
+    avg_platf = {}
+    avg_platf['netflix'] = avg_rating.loc[avg_rating['Netflix'] == 1, ['Title', 'Average Rating']].mean()
+    avg_platf['hulu'] = avg_rating.loc[avg_rating['Hulu'] == 1, ['Title', 'Average Rating']].mean()
+    avg_platf['prime'] = avg_rating.loc[avg_rating['Prime Video'] == 1, ['Title', 'Average Rating']].mean()
+    avg_platf['disney'] = avg_rating.loc[avg_rating['Disney+'] == 1, ['Title', 'Average Rating']].mean()
+    return avg_platf
+
 def main():
     movs_shows = load_movs_shows('data/Movies_Streaming_Platforms.csv',
                                  'data/TvShows_Streaming_Platforms.csv')
