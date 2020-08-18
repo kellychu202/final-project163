87e9fb1dce1cd7dff55f06d5df57d23eb745ed5b
Kelly Chu <kellychu202@gmail.com>
2020-08-18 01:08:52 -0700

    updated genre to one method

--- genre.py
+++ genre.py
@@ -30,110 +30,24 @@ def load_imdb(path_one, path_two):
     return merged
 
 
-def netflix_analysis(df):
-    netflix = df[df['Netflix'] == 1]  
-    new = netflix['Genres'].str.split(",", expand = True)
-    col1 = new[0]
-    col2 = new[1]
-    col3 = new[2]
-    col4 = new[3]
-    col5 = new[4]
-    col6 = new[5]
-    col7 = new[6]
-    col8 = new[7]
-    temp = col1.append(col2)
-    temp2 = temp.append(col4)
-    temp3 = temp2.append(col5)
-    temp4 = temp3.append(col6)
-    temp5 = temp4.append(col7)
-    temp6 = temp5.append(col8)
-    temp6 = temp6.dropna()
-    result = temp6.unique()
-    length = len(result)
-    print(result)
-    print(length)
+def genre_analysis(df, site):
+    df1 = df.loc[df[site] == 1, 'Genres']
+    df1 = df1.dropna()
+    df1 = df1.str.split(',')
+    df1 = df1.explode().unique()
+    length = len(df1)
+    print(df1)
+    print(site, 'has', length, 'genres')
 
-
-def hulu_analysis(df):
-    hulu = df[df['Hulu'] == 1]
-    new = hulu['Genres'].str.split(",", expand = True)
-    col1 = new[0]
-    col2 = new[1]
-    col3 = new[2]
-    col4 = new[3]
-    col5 = new[4]
-    col6 = new[5]
-    col7 = new[6]
-    temp = col1.append(col2)
-    temp2 = temp.append(col4)
-    temp3 = temp2.append(col5)
-    temp4 = temp3.append(col6)
-    temp5 = temp4.append(col7)
-    temp5 = temp5.dropna()
-    result = temp5.unique()
-    length = len(result)
-    print(result)
-    print(length)
-
-
-def disney_analysis(df):
-    disney = df[df['Disney+'] == 1]
-    new = disney['Genres'].str.split(",", expand = True)
-    col1 = new[0]
-    col2 = new[1]
-    col3 = new[2]
-    col4 = new[3]
-    col5 = new[4]
-    col6 = new[5]
-    col7 = new[6]
-    col8 = new[7]
-    temp = col1.append(col2)
-    temp2 = temp.append(col4)
-    temp3 = temp2.append(col5)
-    temp4 = temp3.append(col6)
-    temp5 = temp4.append(col7)
-    temp6 = temp5.append(col8)
-    temp6 = temp6.dropna()
-    result = temp6.unique()
-    length = len(result)
-    print(result)
-    print(length)
-    
-
-
-def prime_analysis(df):
-    prime = df[df['Prime Video'] == 1]
-    new = prime['Genres'].str.split(",", expand = True)
-    col1 = new[0]
-    col2 = new[1]
-    col3 = new[2]
-    col4 = new[3]
-    col5 = new[4]
-    col6 = new[5]
-    col7 = new[6]
-    col8 = new[7]
-    col9 = new[8]
-    temp = col1.append(col2)
-    temp2 = temp.append(col4)
-    temp3 = temp2.append(col5)
-    temp4 = temp3.append(col6)
-    temp5 = temp4.append(col7)
-    temp6 = temp5.append(col8)
-    temp7 = temp6.append(col9)
-    temp7 = temp7.dropna()
-    result = temp7.unique()
-    length = len(result)
-    print(result)
-    print(length)
 
 def main():
     movs_shows = load_movs_shows('data/Movies_Streaming_Platforms.csv',
                                  'data/TvShows_Streaming_Platforms.csv')
-    df = pd.read_csv('data/Movies_Streaming_Platforms.csv')
-    netflix_analysis(df)
-    hulu_analysis(df)
-    disney_analysis(df)
-    prime_analysis(df)
+    movies = pd.read_csv('data/Movies_Streaming_Platforms.csv')
+    genre_analysis(movies, 'Netflix')
+    genre_analysis(movies, 'Hulu')
+    genre_analysis(movies, 'Disney+')
+    genre_analysis(movies, 'Prime Video')
 
 
 if __name__ == '__main__':
